ad5b582f4d4f34b2ee7e2f86c0597d17e8916bf4
Valgus <khuenguyenhaminh@gmail.com>
2025-05-26 13:00:52 +0700

    fix the book_path sys

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import sys
 
 def main():
     # book_path = './books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
     book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = count(text)
     word_dict = into_character(text)
     sort = sort_dict(word_dict)
-    if len(sys.argv) < 2:
-        print('Usage: python3 main.py <path_to_book>')
-        sys.exit(1)
     print('============ BOOKBOT ============')
     print(f'Analyzing book found at {book_path}')
     print('----------- Word Count ----------')
@@ -20,7 +20,7 @@ def main():
     print('--------- Character Count -------')
     for i in sort:
         if i["name"].isalpha() == True:
-            print(f'{i["name"]} : {i["num"]}')
+            print(f'{i["name"]}: {i["num"]}')
     print('============= END ===============')
 
 
