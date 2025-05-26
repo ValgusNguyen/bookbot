1951c5df74954c89b6e1367cd4266ea5e66f71ec
Valgus <khuenguyenhaminh@gmail.com>
2025-05-26 12:21:33 +0700

    change book_path

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from stats import count
 from stats import into_character
 from stats import sort_dict
+import sys
 
 def main():
-    book_path = './books/frankenstein.txt'
+    # book_path = './books/frankenstein.txt'
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = count(text)
     word_dict = into_character(text)
     sort = sort_dict(word_dict)
+    if len(sys.argv) < 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
     print('============ BOOKBOT ============')
     print(f'Analyzing book found at {book_path}')
     print('----------- Word Count ----------')
